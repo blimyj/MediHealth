c4f98408d4227fea5b11b34ea38d8b0adbc813d5
joloong <joelloong@gmail.com>
2019-06-22 12:07:15 +0800

    Fixed merge conflicts.

--- MediHealthProject/app/appStyle.js
+++ MediHealthProject/app/appStyle.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-    bigButton: {
-        alignItems: 'flex-start',
-        backgroundColor: '#28DA9A',
-        width: 270,
-        height: 60,
-        borderRadius: 8,
-        padding: 15,
-        marginTop:10
-      },
-    bigButtonText: {
-        fontSize: 20,
-        fontWeight: '400',
-        color: "#FFFFFF",
-    }
-});
-=======
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
@@ -36,4 +16,3 @@ export default StyleSheet.create({
 		color: "#FFFFFF"
 	}
 });
->>>>>>> c5b1a10da4bb4cb20206f21fd169c1eb484512b1
