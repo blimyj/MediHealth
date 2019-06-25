ea0835b96e4dbd0afa920058a0bdaf8cafeeb0b0
blimyj21@gmail.com <blimyj21@gmail.com>
2019-06-25 10:32:24 +0800

    Made ApptInput updates firebase database and go back to main menu

--- MediHealthProject/app/containers/AppointmentScreen/AppointmentInputScreen/appointmentInputScreen.js
+++ MediHealthProject/app/containers/AppointmentScreen/AppointmentInputScreen/appointmentInputScreen.js
@@ -1,21 +1,48 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ListView } from "react-native";
 import { Form, Input, Item, Label, Container, Content } from "native-base";
 import MyHeader from "../../../components/header";
 import styles from "./appStyle";
 
+import Config from 'react-native-config'
+
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: Config.apiKey,
+    authDomain: Config.authDomain,
+    databaseURL: Config.databaseURL,
+    projectId: Config.projectId,
+    storageBucket: Config.storageBucket,
+    messagingSenderId: Config.messagingSenderId,
+    appId: Config.appId 
+}
+
+firebase.initializeApp(firebaseConfig)
+
+var data = []
+
 class AppointmentInputScreen extends Component {
 	constructor(props) {
-		super(props);
+        super(props);
+        
+        this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+
 		this.state = {
-			appointment: "",
+            listViewData: data,
+            appointment: "",
 			location: "",
 			date: "",
 			time: ""
 		};
 	}
+    
+    addAppointment(dataAppt, dataLocat, dataDate, dataTime) {
+        var key = firebase.database().ref('/items').push().key
+        firebase.database().ref('/items').child(key).set({ appointment: dataAppt, location: dataLocat, date: dataDate, time: dataTime})
+    }
 
-	render() {
+    render() {
 		return (
 			<Container>
 				<MyHeader nav={this.props.navigation} headerTitle="Appointment" />
@@ -30,22 +57,22 @@ class AppointmentInputScreen extends Component {
 						<Item stackedLabel>
 							<Label>Appointment</Label>
 							<Input
-								onSubmitEditing={text => this.setState({ appointment: text })}
+								onChangeText={text => this.setState({ appointment: text })}
 							/>
 						</Item>
 						<Item stackedLabel num1>
 							<Label>Location</Label>
 							<Input
-								onSubmitEditing={text => this.setState({ location: text })}
+								onChangeText={text => this.setState({ location: text })}
 							/>
 						</Item>
 						<Item stackedLabel num2>
 							<Label>Date</Label>
-							<Input onSubmitEditing={text => this.setState({ date: text })} />
+							<Input onChangeText={text => this.setState({ date: text })} />
 						</Item>
 						<Item stackedLabel num3>
 							<Label>Time</Label>
-							<Input onSubmitEditing={text => this.setState({ time: text })} />
+							<Input onChangeText={text => this.setState({ time: text })} />
 						</Item>
 					</Form>
 
@@ -53,7 +80,15 @@ class AppointmentInputScreen extends Component {
 						title="AppointmentInput"
 						style={styles.appointmentInputButton}
 						accessibilityLabel="Appointment Input Button"
-						onPress={() => this.props.navigation.navigate("Appointment")}
+                        onPress={() => { 
+                            this.addAppointment(this.state.appointment,
+                                this.state.location,
+                                this.state.date,
+                                this.state.time
+                            );
+                            this.props.navigation.goBack();
+                            }
+                        }
 					>
 						<Text style={styles.bigButtonText}>+</Text>
 					</TouchableOpacity>
