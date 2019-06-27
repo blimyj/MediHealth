5bb908cf0b4322992a380d1d487030b073ac38b0
joloong <joelloong@gmail.com>
2019-06-27 17:41:22 +0800

    Implemeneted react-native-searchable-dropdown for Map.

--- MediHealthProject/app/containers/MapScreen/mapScreen.js
+++ MediHealthProject/app/containers/MapScreen/mapScreen.js
@@ -6,7 +6,7 @@ import MyHeader from "../../components/header";
 import * as firebase from "firebase";
 import Config from "react-native-config";
 import MapViewDirections from "react-native-maps-directions";
-import { SearchBar } from "react-native-elements";
+import SearchableDropdown from "react-native-searchable-dropdown";
 
 const { width, height } = Dimensions.get("window");
 
@@ -29,11 +29,13 @@ class MapScreen extends Component {
 				longitudeDelta: LONGITUDE_DELTA
 			},
 			mapMargin: 1,
-			markerData: []
+			markerData: [],
+			address: []
 		};
 
 		this.setMargin = this.setMargin.bind(this);
 		this.readUserData = this.readUserData.bind(this);
+		this.changeSelectedRegion = this.changeSelectedRegion.bind(this);
 	}
 
 	setMargin() {
@@ -52,37 +54,81 @@ class MapScreen extends Component {
 				const fbObject = snapshot.val();
 				const newArr = Object.keys(fbObject).map(key => {
 					fbObject[key].id = key;
-					return fbObject[key];
+					return {
+						id: fbObject[key].id,
+						name: fbObject[key]["Pharmacy Name"],
+						Latitude: fbObject[key].Latitude,
+						Longitude: fbObject[key].Longitude,
+						PostalCode: "S" + fbObject[key]["Postal Code"]
+					};
 				});
 				this.setState({ markerData: newArr });
 				console.log(this.state.markerData);
 			});
 	};
 
+	changeSelectedRegion(item) {
+		this.setState({
+			region: {
+				latitude: item.Latitude,
+				longitude: item.Longitude,
+				latitudeDelta: 0.01,
+				longitudeDelta: 0.01
+			}
+		});
+	}
+
 	componentDidMount() {
 		this.readUserData();
 	}
 
 	componentDidUpdate() {
-		this.map.fitToElements(true);
+		// this.map.fitToElements(true);
 	}
 
 	render() {
 		return (
 			<Container>
 				{/* <MyHeader nav={this.props.navigation} headerTitle="Map" /> */}
-				<SearchBar placeholder="Type Here..." />
+				<SearchableDropdown
+					onItemSelect={item => {
+						alert("You've selected " + item.name);
+						this.changeSelectedRegion(item);
+					}}
+					containerStyle={{ padding: 5 }}
+					textInputStyle={{
+						padding: 12,
+						borderWidth: 1,
+						borderColor: "#ccc",
+						borderRadius: 5
+					}}
+					itemStyle={{
+						padding: 10,
+						marginTop: 2,
+						backgroundColor: "#ddd",
+						borderColor: "#bbb",
+						borderWidth: 1,
+						borderRadius: 5
+					}}
+					itemTextStyle={{ color: "#222" }}
+					itemsContainerStyle={{ maxHeight: 140 }}
+					items={this.state.markerData}
+					defaultIndex={2}
+					placeholder="Address"
+					resetValue={false}
+					underlineColorAndroid="transparent"
+				/>
 				<MapView
 					ref={ref => {
 						this.map = ref;
 					}}
 					provider={PROVIDER_GOOGLE}
 					style={{ flex: 1, marginBottom: this.state.mapMargin }}
-					// initialRegion={this.state.region}
 					showsUserLocation={true}
 					showsMyLocationButton={true}
 					followsUserLocation={true} //iOS ONLY
 					onMapReady={this.setMargin}
+					region={this.state.region}
 				>
 					<Marker
 						coordinate={{
@@ -99,7 +145,7 @@ class MapScreen extends Component {
 									latitude: coordinate.Latitude,
 									longitude: coordinate.Longitude
 								}}
-								title={coordinate["Pharmacy Name"]}
+								title={coordinate.name}
 								image={require("../../assets/images/mapMarker-icon.png")}
 								key={coordinate.id}
 							/>
